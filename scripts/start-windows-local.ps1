471794213bb684baccadda956a47500dc424067d
martyw1 <martinwolfe@gmail.com>
2026-06-09 10:06:34 -0400

    Use single Windows preflight in local start wrapper

--- scripts/start-windows-local.ps1
+++ scripts/start-windows-local.ps1
@@ -6,7 +6,5 @@ param(
 )
 
 $ErrorActionPreference = 'Stop'
-& (Join-Path $PSScriptRoot 'preflight-windows.ps1') -AssumeYes:$AssumeYes -SkipFrontendBuild:$SkipFrontendBuild
-if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 & (Join-Path $PSScriptRoot 'startup-windows-local.ps1') -NoBrowser:$NoBrowser -SkipFrontendBuild:$SkipFrontendBuild -AssumeYes:$AssumeYes
 exit $LASTEXITCODE
